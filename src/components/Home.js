358037f2ab8a2dd330895361bc6cda8b56d96512
Xavier Salazar <salazar.xavier26@gmail.com>
2018-10-16 01:29:01 -0500

    Finished home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
-import { Jumbotron, Container, Row, Col } from "reactstrap";
+import {
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Home extends Component {
   render() {
@@ -16,17 +29,58 @@ export default class Home extends Component {
         <Container fluid>
           <Row>
             <Col>
-              <p className="text-center p-5">
+              <h1 className="text-center pt-3">
+                Massage Therapy &amp; Natural Healing
+              </h1>
+              <p className="text-center pt-2 px-5">
                 Come in today for a Singles Massage Session or a Couples
                 Discounted Massage Packet. We use all organic massage products
                 using plants, fresh herbs and oils. We are unlike your everyday
-                Day Spa. You won’t walk in and get a headache from all the
-                chemicals from the nails and hair. And during your massage you
-                won’t be bothered with the noise of others talking. We are a
-                private practice not corporate. We cater to just you when you
-                arrive. We help ease all your aches and pains helping you find
-                true Peace Of Mind.
+                Day Spa. We are a private practice not corporate. We cater to
+                just you when you arrive. We help ease all your aches and pains
+                helping you find true Peace Of Mind.
               </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h1 className="text-center pt-5 px-5">Testimonials</h1>
+              <Card>
+                <CardBody>
+                  <CardTitle className="px-5 pt-2">Miranda B.</CardTitle>
+                  <CardText className="px-5">
+                    My experience was phenomenal! Jynelle &amp; Natalie went
+                    above and beyond to make me feel relaxed and comfortable.
+                    They took their time and put in a lot of effort with each
+                    service I got done. Thank you both so much. I’m a mother to
+                    a 11 month old, i haven’t pampered myself lately and today
+                    y’all made me feel so so relaxed. I will be going back. I
+                    recommend getting a facial, full body massage and pedicure
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody>
+                  <CardTitle className="px-5 pt-2">Rich WIIIZ</CardTitle>
+                  <CardText className="px-5">
+                    Great place, very professional and relaxing environment,
+                    very understanding of the body and they work well with
+                    injuries. I am definitely going to sleep like a baby
+                    tonight, great deep tissue massage. I highly recommend them
+                    to anyone in the area.
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody>
+                  <CardTitle className="px-5 pt-2">Sarah Ortiz</CardTitle>
+                  <CardText className="px-5">
+                    Calming environment, highly qualified staff, reasonable
+                    prices, and great attention was paid to the products used. I
+                    did recommend this place to friends and family.
+                  </CardText>
+                </CardBody>
+              </Card>
             </Col>
           </Row>
         </Container>
