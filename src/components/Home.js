c3c92d4f3b61e5e728554489872e60c643658c4a
Xavier Salazar <salazar.xavier26@gmail.com>
2018-10-22 20:10:24 -0500

    styling services section of home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Jumbotron, Container, Row, Col } from "reactstrap";
+import { Jumbotron, Container, Row, Col, Button } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Home extends Component {
@@ -33,8 +33,88 @@ export default class Home extends Component {
 
           <Row className="odd-row py-5">
             <Col>
-              <h1 className="text-center px-5">Testimonials</h1>
+              <h1 className="text-center px-5">Services</h1>
             </Col>
+            <Container className="services-container">
+              <Row className="text-center">
+                <Col xs={12} md={4} className="service-col">
+                  <i class="fas fa-hand-holding-heart service-icons" />
+                  <h6>Massages</h6>
+                  <p>You will leave completely rejuvenated</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+                <Col xs={12} md={4} className="service-col">
+                  <i class="fas fa-users service-icons" />
+                  <h6>Deluxe Couples</h6>
+                  <p>Stimulating massages for the couples</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+                <Col xs={12} md={4} className="service-col">
+                  <i class="far fa-laugh-beam service-icons" />
+                  <h6>Facials</h6>
+                  <p>Treatments surely to make your face radiate</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+              </Row>
+              <Row className="text-center">
+                <Col xs={12} md={4} className="service-col">
+                  <i class="fas fa-diagnoses service-icons" />
+                  <h6>Waxes</h6>
+                  <p>Full service waxing</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+                <Col xs={12} md={4} className="service-col">
+                  <i class="fas fa-hands service-icons" />
+                  <h6>Scrubs/Weight Loss</h6>
+                  <p>Our deep scrubs and weight loss treatments</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+                <Col xs={12} md={4} className="service-col">
+                  <i class="far fa-heart service-icons" />
+                  <h6>Infrared Sauna</h6>
+                  <p>Infrared light therapy to help you heal</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+              </Row>
+              <Row className="text-center">
+                <Col xs={12} md={4} className="service-col">
+                  <i class="fas fa-brain service-icons" />
+                  <h6>Oxygen Bar</h6>
+                  <p>Oxygen rejuvenation for the brain</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+                <Col xs={12} md={4} className="service-col">
+                  <i class="fas fa-shoe-prints service-icons" />
+                  <h6>Oxygen Foot Soak</h6>
+                  <p>Detox the impurities from your body</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+                <Col xs={12} md={4} className="service-col">
+                  <i class="fas fa-plus service-icons" />
+                  <h6>Add On Services</h6>
+                  <p>Services to add on your massages</p>
+                  <a href="#" className="service-button">
+                    More Info
+                  </a>
+                </Col>
+              </Row>
+            </Container>
           </Row>
         </Container>
       </Container>
