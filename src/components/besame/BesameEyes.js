cb53695b3e34359d3f8801d9436cd93676773af6
Xavier Salazar <salazar.xavier26@gmail.com>
2019-02-03 19:41:34 -0600

    Finishing BesameEyes

--- src/components/besame/BesameEyes.js
+++ src/components/besame/BesameEyes.js
@@ -1,9 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import styled from "styled-components";
+
+const MainContainer = styled(Container)`
+  margin: 6rem auto 3rem auto;
+`;
+
+const Eye = styled.img`
+  margin: 10px 0;
+  border-radius: 50%;
+`;
+
+const Disclaimer = styled.p`
+  text-align: center;
+  margin-top: 50px;
+`;
 
 export default class BesameEyes extends Component {
   render() {
     return (
-      <h1>Besame Eyes</h1>
+      <MainContainer fluid>
+        <Col xs={12} className="mt-5">
+          <h1 className="text-center pb-3">Eyes</h1>
+        </Col>
+        <Row className="text-center">
+          <Col xs={12} md={4}>
+            <Eye
+              src="/resources/eyes/black-mascara.jpg"
+              className="img-fluid"
+            />
+            <p>Black Mascara</p>
+          </Col>
+          <Col xs={12} md={4}>
+            <Eye src="/resources/eyes/mascara.jpg" className="img-fluid" />
+            <p>Brown Cake Mascara</p>
+          </Col>
+          <Col xs={12} md={4}>
+            <Eye src="/resources/eyes/mascara.jpg" className="img-fluid" />
+            <p>Black Cake Mascara</p>
+          </Col>
+        </Row>
+        <Disclaimer>All products sold in store only</Disclaimer>
+      </MainContainer>
     );
   }
 }
