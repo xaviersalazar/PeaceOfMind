dd0df304690096be2464af0e113395e1f7e1c4ad
Xavier Salazar <salazar.xavier26@gmail.com>
2019-04-04 18:42:09 -0500

    Finishing SpecialtyExtensions

--- src/components/salon/SpecialtyExtensions.js
+++ src/components/salon/SpecialtyExtensions.js
@@ -26,6 +26,35 @@ const Disclaimer = styled.p`
   margin-top: 50px;
 `;
 
+const specialtyExtensions = [
+  {
+    row: [
+      {
+        icon: "/resources/icons/salon/specialty/kinky.png",
+        title: "Kinky Twist",
+        price: "$150.00",
+        desc: ""
+      },
+      {
+        icon: "/resources/icons/salon/specialty/rope.png",
+        title: "Rope Twist",
+        price: "$170.00",
+        desc: ""
+      }
+    ]
+  },
+  {
+    row: [
+      {
+        icon: "/resources/icons/salon/specialty/micro.png",
+        title: "Micro Braids",
+        price: "$300.00 - $450.00",
+        desc: ""
+      }
+    ]
+  }
+];
+
 export default class SpecialtyExtensions extends Component {
   render() {
     return (
@@ -33,6 +62,7 @@ export default class SpecialtyExtensions extends Component {
         <ParagraphsCol xs={12} className="mt-5">
           <ServiceTitle>Specialty w/ Extensions</ServiceTitle>
         </ParagraphsCol>
+        <ServiceRows rows={specialtyExtensions} />
         <Row>
           <ParagraphsCol xs={12}>
             <Disclaimer>
