887a3fc82f962337d00fdfebcbfbe38c5cd8b4c7
Xavier Salazar <salazar.xavier26@gmail.com>
2019-04-03 23:30:12 -0500

    Finishing Relaxer

--- src/components/salon/Relaxer.js
+++ src/components/salon/Relaxer.js
@@ -26,6 +26,57 @@ const Disclaimer = styled.p`
   margin-top: 50px;
 `;
 
+const relaxers = [
+  {
+    row: [
+      {
+        icon: "/resources/icons/salon/relaxer/relax.png",
+        title: "Relaxer",
+        price: "$100.00 - $120.00",
+        desc: ""
+      },
+      {
+        icon: "/resources/icons/salon/haircuts/haircut.png",
+        title: "Relaxer w/ Haircut",
+        price: "$135.00 - $145.00",
+        desc: ""
+      }
+    ]
+  },
+  {
+    row: [
+      {
+        icon: "/resources/icons/salon/color/haircolor5.png",
+        title: "Relaxer w/ Color",
+        price: "$150.00 - $200.00",
+        desc: ""
+      },
+      {
+        icon: "/resources/icons/salon/highlighting/ombre-haircut.png",
+        title: "Relaxer w/ Haircut & Color",
+        price: "$170.00 - $225.00",
+        desc: ""
+      }
+    ]
+  },
+  {
+    row: [
+      {
+        icon: "/resources/icons/salon/relaxer/retouch.png",
+        title: "Retouch",
+        price: "$55.00 - $65.00",
+        desc: 'Up to 1"'
+      },
+      {
+        icon: "/resources/icons/salon/haircuts/bandtrim.png",
+        title: "Retouch w/ Haircut",
+        price: "$80.00 - $100.00",
+        desc: 'Up to 1"'
+      }
+    ]
+  }
+];
+
 export default class Relaxer extends Component {
   render() {
     return (
@@ -33,6 +84,7 @@ export default class Relaxer extends Component {
         <ParagraphsCol xs={12} className="mt-5">
           <ServiceTitle>Relaxer</ServiceTitle>
         </ParagraphsCol>
+        <ServiceRows rows={relaxers} />
         <Row>
           <ParagraphsCol xs={12}>
             <Disclaimer>
