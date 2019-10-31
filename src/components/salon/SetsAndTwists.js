d06e05d4e9abdd8e1143f392ed5daf4b884ac108
Xavier Salazar <salazar.xavier26@gmail.com>
2019-10-31 08:34:50 -0500

    redesign sets

--- src/components/salon/SetsAndTwists.js
+++ src/components/salon/SetsAndTwists.js
@@ -1,11 +1,57 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 import styled from "styled-components";
-import ServiceRows from "../common/ServiceRows";
+import ServiceCardRow from "../common/ServiceCardRow";
 
 const MainContainer = styled(Container)`
-  padding-top: 5rem;
+  padding: 5rem 2.5rem 0 2.5rem !important;
   background: #f3eff5;
+
+  @media (min-width: 768px) {
+    background-attachment: fixed;
+    padding: 5rem 3.5rem 0 3.5rem !important;
+  }
+
+  @media (min-width: 992px) {
+    padding: 5rem 5rem 0 5rem !important;
+  }
+`;
+
+const Background = styled.div`
+  background-image: url("/resources/services/salon/sets-main.jpg");
+  height: 50vh;
+  width: 100vw;
+  min-width: 100vw;
+  background-attachment: scroll;
+  background-position: bottom center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin: 0;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    background-attachment: fixed;
+  }
+`;
+
+const Title = styled.div`
+  position: absolute;
+  top: 25%;
+  right: 50%;
+  transform: translateX(50%);
+  font-size: 4.5rem;
+  color: #fff !important;
+`;
+
+const Hr = styled.hr`
+  background: #fff;
+  padding: 0;
+  width: 80%;
+  margin: 0 auto;
+
+  @media (min-width: 992px) {
+    width: 81%;
+  }
 `;
 
 const ParagraphsCol = styled(Col)`
@@ -17,8 +63,11 @@ const ParagraphsCol = styled(Col)`
 `;
 
 const ServiceTitle = styled.h1`
+  color: #fff !important;
+  font-size: 3.5rem;
+  margin: 0;
+  padding: 0;
   text-align: center;
-  padding: 5rem 0 3rem 0;
 `;
 
 const Disclaimer = styled.p`
@@ -30,58 +79,71 @@ const setsAndTwists = [
   {
     row: [
       {
-        icon: "/resources/icons/salon/setsandtwists/pressandcurl.png",
+        icon: "/resources/services/salon/sets-press-curl.jpg",
         title: "Press and Curl",
         price: "$100.00 - $150.00",
-        desc: ""
+        desc: "",
+        sm: 12,
+        md: 6,
+        lg: 3
       },
       {
-        icon: "/resources/icons/salon/setsandtwists/shampoo.png",
+        icon: "/resources/services/salon/sets-shampoo-rinse.jpg",
         title: "Shampoo Services w/ Two-Stand Twist",
         price: "$95.00 - $110.00",
-        desc: ""
-      }
-    ]
-  },
-  {
-    row: [
+        desc: "",
+        sm: 12,
+        md: 6,
+        lg: 3
+      },
       {
-        icon: "/resources/icons/salon/setsandtwists/shampoo2.png",
+        icon: "/resources/services/salon/sets-shampoo-rinse-flat.jpg",
         title: "Shampoo Services w/ Flat-Twist Set",
         price: "$95.00 - $110.00",
-        desc: ""
+        desc: "",
+        sm: 12,
+        md: 6,
+        lg: 3
       },
       {
-        icon: "/resources/icons/salon/setsandtwists/singletwist.png",
+        icon: "/resources/services/salon/sets-single-twist.jpg",
         title: "Single Twist Comb Twist",
         price: "$90.00 - $200.00",
-        desc: ""
+        desc: "",
+        sm: 12,
+        md: 6,
+        lg: 3
       }
     ]
   },
   {
     row: [
       {
-        icon: "/resources/icons/salon/setsandtwists/doubletwist.png",
+        icon: "/resources/services/salon/sets-double-twist.jpg",
         title: "Double Twist Two Strand",
         price: "$90.00 - $200.00",
-        desc: ""
+        desc: "",
+        sm: 12,
+        md: 6,
+        lg: 3
       },
       {
-        icon: "/resources/icons/salon/setsandtwists/straw.png",
+        icon: "/resources/services/salon/sets-straw.jpg",
         title: "Straw Set",
         price: "$90.00 - $150.00",
-        desc: ""
-      }
-    ]
-  },
-  {
-    row: [
+        desc: "",
+        sm: 12,
+        md: 6,
+        lg: 3
+      },
       {
-        icon: "/resources/icons/salon/setsandtwists/rod.png",
+        icon: "/resources/services/salon/sets-rod.jpg",
         title: "Rod Set",
         price: "$90.00 - $150.00",
-        desc: ""
+        desc: "",
+        sm: 12,
+        md: 6,
+        lg: 3
       }
     ]
   }
@@ -90,19 +152,23 @@ const setsAndTwists = [
 export default class SetsAndTwists extends Component {
   render() {
     return (
-      <MainContainer fluid>
-        <ParagraphsCol xs={12} className="mt-5">
+      <div>
+        <Title>
           <ServiceTitle>Sets &amp; Twists</ServiceTitle>
-        </ParagraphsCol>
-        <ServiceRows rows={setsAndTwists} />
-        <Row>
-          <ParagraphsCol xs={12}>
-            <Disclaimer>
-              NOTICE: Prices Are Subject To Change Without Prior Notice
-            </Disclaimer>
-          </ParagraphsCol>
-        </Row>
-      </MainContainer>
+          <Hr />
+        </Title>
+        <Background />
+        <MainContainer fluid>
+          <ServiceCardRow rows={setsAndTwists} />
+          <Row>
+            <ParagraphsCol xs={12}>
+              <Disclaimer>
+                NOTICE: Prices Are Subject To Change Without Prior Notice
+              </Disclaimer>
+            </ParagraphsCol>
+          </Row>
+        </MainContainer>
+      </div>
     );
   }
 }
