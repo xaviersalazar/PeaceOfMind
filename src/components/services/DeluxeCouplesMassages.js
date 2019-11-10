d29b67b0bc71d778c5e88acbaef5dc5a1593b0c8
Xavier Salazar <salazar.xavier26@gmail.com>
2019-11-09 21:50:04 -0600

    redesign deluxe-couples

--- src/components/services/DeluxeCouplesMassages.js
+++ src/components/services/DeluxeCouplesMassages.js
@@ -1,83 +1,74 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
-import styled from "styled-components";
-import ServiceRows from "../common/ServiceRows";
+import ServicePage from "../common/ServicePage";
 
-const MainContainer = styled(Container)`
-  padding-top: 5rem;
-  background: #f3eff5;
-`;
-
-const ServiceTitle = styled.h1`
-  text-align: center;
-  padding: 5rem 0;
-`;
-
-const Disclaimer = styled.p`
-  text-align: center;
-  margin-top: 50px;
-`;
-
-const ParagraphsCol = styled(Col)`
-  padding: 0 3rem;
-
-  @media (min-width: 1440px) {
-    padding: 0 10rem !important;
-  }
-`;
-
+const background = "/resources/services/massages/massage-couples-main.jpg";
+const title = "Deluxe/Couples Massages";
 const deluxeCouplesMassages = [
   {
     row: [
       {
-        icon: "/resources/icons/couples/couple1.png",
+        icon: "/resources/services/massages/massage-couples-1.jpg",
         title: "Couples #1",
         price: "1 Hour: $247.00 | Upgrade to Deep Tissue: $297.00",
         desc:
-          "Swedish Massage with Aromatherapy.Performed in a serene setting, the duet massage encourages unity,connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Swedish Massages that help the two of you relax. With a Cooling Mint Eye mask. Including reflexology and a scalp massage. Breathing treatment & Herbal infused hot towel with a foot detox herbal spray."
+          "Swedish Massage with Aromatherapy.Performed in a serene setting, the duet massage encourages unity,connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Swedish Massages that help the two of you relax. With a Cooling Mint Eye mask. Including reflexology and a scalp massage. Breathing treatment & Herbal infused hot towel with a foot detox herbal spray.",
+        sm: 12,
+        md: 6,
+        lg: 3
       },
       {
-        icon: "/resources/icons/couples/couple1.png",
+        icon: "/resources/services/massages/massage-couples-2.jpg",
         title: "Couples #2",
         price: "1 Hour & 30 mins: $480.00",
         desc:
-          "Hot Stones & Aromatherapy. Performed in a serene setting, the duet massage encourages unity, connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Swedish Massages that help the two of you relax.With a Cooling Mint Eye mask. Including reflexology and a scalp massage. Breathing treatment, with a Herbal infused detox spray."
-      }
-    ]
-  },
-  {
-    row: [
+          "Hot Stones & Aromatherapy. Performed in a serene setting, the duet massage encourages unity, connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Swedish Massages that help the two of you relax.With a Cooling Mint Eye mask. Including reflexology and a scalp massage. Breathing treatment, with a Herbal infused detox spray.",
+        sm: 12,
+        md: 6,
+        lg: 3
+      },
       {
-        icon: "/resources/icons/couples/couple3.png",
+        icon: "/resources/services/massages/massage-couples-3.jpg",
         title: "Couples #3",
         price: "1 Hour & 30 mins: $480.00",
         desc:
-          "Deep Tissue Massage, Hot Stone, Aromatherapy & Indian Scalp Massage. Performed in a serene setting,the duet massage encourages unity, connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Deep Tissue Massages that help the two of you relax.With a Cooling Eye mask. With our own Herbal breathing treatment and herbal foot detox spray."
+          "Deep Tissue Massage, Hot Stone, Aromatherapy & Indian Scalp Massage. Performed in a serene setting,the duet massage encourages unity, connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Deep Tissue Massages that help the two of you relax.With a Cooling Eye mask. With our own Herbal breathing treatment and herbal foot detox spray.",
+        sm: 12,
+        md: 6,
+        lg: 3
       },
       {
-        icon: "/resources/icons/couples/pampered.png",
+        icon: "/resources/services/massages/massage-deluxe-party.jpg",
         title: "Pampered Party Packet",
         price: "$350.00 Per Person (Party of 3 or More)",
         desc:
-          "1 Hour and 35 Minutes of Swedish Massage, Hot Stones, Reflexology, Aromatherapy, and a Pressure Point Release Rain Drop Facial and Pearl Mask."
+          "1 Hour and 35 Minutes of Swedish Massage, Hot Stones, Reflexology, Aromatherapy, and a Pressure Point Release Rain Drop Facial and Pearl Mask.",
+        sm: 12,
+        md: 6,
+        lg: 3
       }
     ]
   },
   {
     row: [
       {
-        icon: "/resources/icons/couples/silk.png",
+        icon: "/resources/services/massages/massage-deluxe-silk-spine.jpg",
         title: "Silk Spine Massage",
         price: "10 Minutes: $30.00",
         desc:
-          "Gliding around spine very gently pushing tired knotted up muscles away from the spine gently caressing the nerves in the surrounding are to relive and relax your central nervous system. Allowing your central nervous system to flow freely and unblocked. Better sleep and fewer headaches along with lowered anxiety. Overall mood improver!"
+          "Gliding around spine very gently pushing tired knotted up muscles away from the spine gently caressing the nerves in the surrounding are to relive and relax your central nervous system. Allowing your central nervous system to flow freely and unblocked. Better sleep and fewer headaches along with lowered anxiety. Overall mood improver!",
+        sm: 12,
+        md: 6,
+        lg: 3
       },
       {
-        icon: "/resources/icons/couples/fourhand.png",
+        icon: "/resources/services/massages/massage-deluxe-4-handed.jpg",
         title: "4 Handed Massage",
         price: "30 Minutes: $90.00 | 1 Hour: $180.00 | 1 Hour ½: $270.00",
         desc:
-          "A therapeutic, duet massage is performed by two therapists whose coordinated movements induce relaxation and ease tension. To promote self balance and clarity. A soothing scalp massage and reflexology will leave you with a sense of calm and well-being. The organic,balancing massage oil used during treatment soothes the senses and nourishes the skin. Emerge renewed, refreshed and relaxed."
+          "A therapeutic, duet massage is performed by two therapists whose coordinated movements induce relaxation and ease tension. To promote self balance and clarity. A soothing scalp massage and reflexology will leave you with a sense of calm and well-being. The organic,balancing massage oil used during treatment soothes the senses and nourishes the skin. Emerge renewed, refreshed and relaxed.",
+        sm: 12,
+        md: 6,
+        lg: 3
       }
     ]
   }
@@ -86,21 +77,11 @@ const deluxeCouplesMassages = [
 export default class DeluxeCouplesMassages extends Component {
   render() {
     return (
-      <MainContainer fluid>
-        <ParagraphsCol xs={12} className="mt-5">
-          <ServiceTitle>
-            Invigorating Massages and Couples Massages
-          </ServiceTitle>
-        </ParagraphsCol>
-        <ServiceRows rows={deluxeCouplesMassages} />
-        <Row>
-          <ParagraphsCol xs={12}>
-            <Disclaimer>
-              NOTICE: Prices Are Subject To Change Without Prior Notice
-            </Disclaimer>
-          </ParagraphsCol>
-        </Row>
-      </MainContainer>
+      <ServicePage
+        background={background}
+        title={title}
+        rows={deluxeCouplesMassages}
+      />
     );
   }
 }
