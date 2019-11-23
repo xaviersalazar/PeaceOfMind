98d356d7459a171de789719cecae4716c96c39ff
Xavier Salazar <salazar.xavier26@gmail.com>
2019-11-23 10:28:09 -0600

    redesign about salon

--- src/components/salon/Salon.js
+++ src/components/salon/Salon.js
@@ -1,185 +1,218 @@
 import React, { Component } from "react";
-import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
-import styled from "styled-components";
+import { Container, Row, Col } from "reactstrap";
+import styled, { keyframes } from "styled-components";
+import MassageBenefitsCardRow from "../common/MassageBenefitsCardRow";
 
-const MainContainer = styled(Container)`
-  padding-top: 5rem;
-  background: #f3eff5;
-`;
-
-const ParagraphsCol = styled(Col)`
-  padding: 0 3rem;
-
-  @media (min-width: 1440px) {
-    padding: 0 10rem !important;
+// Keyframes
+const expandHr = keyframes`
+  from {
+    width: 0%;
+  } to {
+    width: 50%
   }
 `;
 
-const ServiceIcon = styled.img`
-  position: relative;
-  margin: 25px 0 0 0;
-  border-radius: 50%;
-  box-shadow: 0 3px 15px rgba(0, 0, 0, 0.12);
-  width: 35%;
-  left: 50%;
-  transform: translateX(-50%);
+// Styles
+const MainContainer = styled(Container)`
+  padding: 5rem 3rem !important;
+  background: #f3eff5;
 
-  @media (min-width: 576px) {
-    width: 20%;
+  @media (min-width: 768px) {
+    background-attachment: fixed;
+    padding: 5rem 5rem !important;
   }
 
   @media (min-width: 992px) {
-    width: 8%;
+    padding: 5rem 8rem !important;
   }
 `;
 
-const ServiceTitle = styled.h1`
-  text-align: center;
-  padding: 5rem 0 3rem 0;
+const Background = styled.div`
+  background-image: url("${props => props.background}");
+  height: 50vh;
+  width: 100vw;
+  min-width: 100vw;
+  background-attachment: scroll;
+  background-position: bottom center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin: 0;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    background-attachment: fixed;
+  }
 `;
 
-const SectionTitle = styled.h4`
-  text-align: center;
-  margin-top: 3rem;
+const Title = styled.div`
+  position: absolute;
+  top: 25%;
+  right: 50%;
+  transform: translateX(50%);
+  color: #fff !important;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 `;
 
-const CustomListGroup = styled(ListGroup)`
-  border: none;
+const Hr = styled.hr`
+  background: #fff;
+  padding: 0;
   width: 50%;
   margin: 0 auto;
+  animation: ${expandHr} 3s linear;
+`;
+
+const ParagraphsCol = styled(Col)`
+  padding: 0;
+`;
+
+const ServiceTitle = styled.h1`
+  color: #fff !important;
+  font-size: 3rem;
+  margin: 0;
+  padding: 0;
   text-align: center;
 `;
 
-const CustomListGroupItem = styled(ListGroupItem)`
-  border: none !important;
-  background: #f3eff5 !important;
-  font-weight: 200;
-  padding: 0 0 5px 0 !important;
+const HeadingHr = styled.hr`
+  width: 50%;
+
+  @media (min-width: 768px) {
+    width: 25%;
+  }
 `;
 
-const ColoredAt = styled.span`
-  background: linear-gradient(-45deg, #b3ffab, #12fff7);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-`;
-
-const systemBenefits = [
-  "No nasty chemical smell.",
-  "Cruelty-free.",
-  "No damage to your client’s hair.",
-  "Complete grey coverage.",
-  "A healthier salon environment.",
-  "A unique talking point with your clients.",
-  "An ethical product range which you can use ‘guilt free’.",
-  "Offer a personalised service for each and every client.",
-  "Stand out from the competition.",
-  "Attract new audiences of clients."
+// TODO: Change picture of benfits
+const benefits = [
+  {
+    row: [
+      {
+        icon: "/resources/services/salon/hair-ext.jpg",
+        title: "Organic Hair Colour Systems® Benefits ",
+        items: [
+          "No nasty chemical smell",
+          "Cruelty-free",
+          "No damage to your client’s hair",
+          "Complete grey coverage",
+          "A healthier salon environment",
+          "A unique talking point with your clients",
+          "An ethical product range which you can use ‘guilt free’",
+          "Offer a personalised service for each and every client",
+          "Stand out from the competition",
+          "Attract new audiences of clients"
+        ],
+        sm: 12,
+        xl: 4
+      }
+    ]
+  }
 ];
 
+// TODO: Change background of main
 export default class Salon extends Component {
   render() {
     return (
-      <MainContainer fluid>
-        <ParagraphsCol xs={12} className="mt-5">
-          <ServiceTitle>
-            Salon <ColoredAt>@</ColoredAt> Peace Of Mind
-          </ServiceTitle>
-        </ParagraphsCol>
-        <Row>
-          <ParagraphsCol xs={12} className="mb-3">
-            <ServiceIcon
-              src="/resources/icons/salon/salon.png"
-              className="img-fluid mb-3"
-            />
-            <SectionTitle style={{ marginTop: "1rem" }}>
-              Organic Hair Colour Systems
-            </SectionTitle>
-            <p style={{ textAlign: "center" }}>
-              This color system gives hairdressers a less toxic and more
-              naturally based salon professional color, care and styling range.
-              Co-founded by hairdresser Stephen Landreth, who was forced to give
-              up hairdressing when he developed a severe allergy to conventional
-              hair color, soon realized there were not many alternatives
-              available. The solution? He made it himself. After years of
-              research and development Organic Colour Systems was born.
-            </p>
-            <p style={{ textAlign: "center" }}>
-              Our dermatologically tested and patented permanent hair color
-              formula, contains the lowest possible levels of oxidative pigment
-              (PPD/PTD) and is packed full of high grade, natural and certified
-              organic ingredients. Uniquely, our permanent color works at a
-              lower level pH than traditional chemical colors, meaning that
-              there is no damage to the hair. The System is supported by our
-              class-leading range of hair care products, which repair and
-              restore the health of your clients’ hair before you even begin to
-              color, then re-balance and close the cuticle of the hair after
-              coloring.
-            </p>
-          </ParagraphsCol>
-        </Row>
-        <Row>
-          <ParagraphsCol xs={12} className="mb-3">
-            <ServiceIcon
-              src="/resources/icons/salon/haircolor.png"
-              className="img-fluid"
-            />
-            <SectionTitle style={{ marginTop: "1rem" }}>
-              This system offers a follicle strengthening system called Rep-Hair
-            </SectionTitle>
-            <p style={{ textAlign: "center" }}>
-              Rep-Hair® Follicle Strengthening System is a natural and
-              scientifically formulated daily scalp and hair care regime,
-              designed to improve the strength and condition of the scalp and
-              hair, providing an optimum follicular environment for hair growth.
-              Rep-Hair® combines natural and organic extracts which have been
-              carefully selected for their nourishing, strengthening,
-              conditioning and cleansing properties, as well as their ability to
-              protect the scalp and reduce inflammation of the hair follicle.
-              This highly effective and natural system harnesses the unique
-              benefits of Capixyl™, an elite hair care complex which has been
-              proven to reduce hair loss and stimulate natural hair growth. If
-              used continuously as directed, noticeable results should be seen
-              within 4 – 16 weeks.
-            </p>
-            <p style={{ textAlign: "center" }}>
-              Our unique system &amp; green science. Revolutionary , high
-              performance and unlike any other professional line. Organic Colour
-              Systems is exactly that, a ‘System’. Our range of high
-              performance, professional salon products, have been designed to
-              work harmoniously with one another to give you and your clients
-              outstanding results in the most natural way possible. Our
-              dermatologically tested and patented permanent hair colour
-              formula, contains the lowest possible levels of oxidative pigment
-              (PPD/PTD) and is packed full of high grade, natural and certified
-              organic ingredients. Uniquely, our permanent colour works at a
-              lower level pH than traditional chemical colours, meaning that
-              there is no damage to the hair. The System is supported by our
-              class-leading range of hair care products, which repair and
-              restore the health of your clients’ hair before you even begin to
-              colour, then re-balance and close the cuticle of the hair after
-              colouring.
-            </p>
-          </ParagraphsCol>
-        </Row>
-        <Row>
-          <ParagraphsCol xs={12} className="mb-3">
-            <ServiceIcon
-              src="/resources/icons/salon/benefits.png"
-              className="img-fluid"
-            />
-            <SectionTitle style={{ marginTop: "1rem" }}>
-              System Benefits
-            </SectionTitle>
-            <CustomListGroup flush>
-              {systemBenefits.map(b => (
-                <CustomListGroupItem>
-                  <i className="fas fa-plus" style={{ fontSize: "8px" }} /> {b}
-                </CustomListGroupItem>
-              ))}
-            </CustomListGroup>
-          </ParagraphsCol>
-        </Row>
-      </MainContainer>
+      <div>
+        <Title>
+          <ServiceTitle>About our Salon</ServiceTitle>
+          <Hr />
+        </Title>
+        <Background
+          background={"/resources/services/salon/finishing-touches-main.jpg"}
+        />
+        <MainContainer fluid>
+          <Row className="mb-5">
+            <ParagraphsCol xs={12} className="text-center">
+              <h1 className="text-center">We use trusted organic products</h1>
+              <HeadingHr />
+              <ParagraphsCol xs={12} className="text-center">
+                <p>
+                  That trusted product is Organic Hair Colour Systems®. This
+                  color system gives hairdressers a less toxic and more
+                  naturally based salon professional color, care and styling
+                  range. Co-founded by hairdresser Stephen Landreth, who was
+                  forced to give up hairdressing when he developed a severe
+                  allergy to conventional hair color, soon realized there were
+                  not many alternatives available. The solution? He made it
+                  himself. After years of research and development Organic
+                  Colour Systems was born.
+                </p>
+              </ParagraphsCol>
+              <ParagraphsCol xs={12} className="text-center">
+                <p>
+                  Our dermatologically tested and patented permanent hair color
+                  formula, contains the lowest possible levels of oxidative
+                  pigment (PPD/PTD) and is packed full of high grade, natural
+                  and certified organic ingredients. Uniquely, our permanent
+                  color works at a lower level pH than traditional chemical
+                  colors, meaning that there is no damage to the hair. The
+                  System is supported by our class-leading range of hair care
+                  products, which repair and restore the health of your clients’
+                  hair before you even begin to color, then re-balance and close
+                  the cuticle of the hair after coloring.
+                </p>
+              </ParagraphsCol>
+              <ParagraphsCol>
+                <p>
+                  This system also offers a follicle strengthening system called
+                  Rep-Hair®. Our dermatologically tested and patented permanent
+                  hair color formula, contains the lowest possible levels of
+                  oxidative pigment (PPD/PTD) and is packed full of high grade,
+                  natural and certified organic ingredients. Uniquely, our
+                  permanent color works at a lower level pH than traditional
+                  chemical colors, meaning that there is no damage to the hair.
+                  The System is supported by our class-leading range of hair
+                  care products, which repair and restore the health of your
+                  clients’ hair before you even begin to color, then re-balance
+                  and close the cuticle of the hair after coloring.
+                </p>
+              </ParagraphsCol>
+              <ParagraphsCol>
+                <p>
+                  Rep-Hair® Follicle Strengthening System is a natural and
+                  scientifically formulated daily scalp and hair care regime,
+                  designed to improve the strength and condition of the scalp
+                  and hair, providing an optimum follicular environment for hair
+                  growth. Rep-Hair® combines natural and organic extracts which
+                  have been carefully selected for their nourishing,
+                  strengthening, conditioning and cleansing properties, as well
+                  as their ability to protect the scalp and reduce inflammation
+                  of the hair follicle. This highly effective and natural system
+                  harnesses the unique benefits of Capixyl™, an elite hair care
+                  complex which has been proven to reduce hair loss and
+                  stimulate natural hair growth. If used continuously as
+                  directed, noticeable results should be seen within 4 – 16
+                  weeks.
+                </p>
+              </ParagraphsCol>
+              <ParagraphsCol>
+                <p>
+                  Our unique system &amp; green science is revolutionary, high
+                  performance and unlike any other professional line. Organic
+                  Colour Systems® is exactly that, a ‘System’. Our range of high
+                  performance, professional salon products, have been designed
+                  to work harmoniously with one another to give you and your
+                  clients outstanding results in the most natural way possible.
+                  Our dermatologically tested and patented permanent hair colour
+                  formula, contains the lowest possible levels of oxidative
+                  pigment (PPD/PTD) and is packed full of high grade, natural
+                  and certified organic ingredients. Uniquely, our permanent
+                  colour works at a lower level pH than traditional chemical
+                  colours, meaning that there is no damage to the hair. The
+                  System is supported by our class-leading range of hair care
+                  products, which repair and restore the health of your clients’
+                  hair before you even begin to colour, then re-balance and
+                  close the cuticle of the hair after colouring.
+                </p>
+              </ParagraphsCol>
+            </ParagraphsCol>
+          </Row>
+          <MassageBenefitsCardRow rows={benefits} />
+        </MainContainer>
+      </div>
     );
   }
 }
