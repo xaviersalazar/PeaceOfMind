4c9568349a2fb72eb4d3292f4523a69c7b0d8292
Xavier Salazar <salazar.xavier26@gmail.com>
2020-08-07 21:37:23 -0500

    updating prices

--- src/components/services/DeluxeCouplesMassages.js
+++ src/components/services/DeluxeCouplesMassages.js
@@ -9,31 +9,31 @@ const deluxeCouplesMassages = [
       {
         icon: "/resources/services/massages/massage-couples-1.jpg",
         title: "Couples #1",
-        price: "1 Hour: $247.00 | Upgrade to Deep Tissue: $297.00",
+        price: "1 Hour: $267.00 | Upgrade to Deep Tissue: $317.00",
         desc:
           "Swedish Massage with Aromatherapy.Performed in a serene setting, the duet massage encourages unity,connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Swedish Massages that help the two of you relax. With a Cooling Mint Eye mask. Including reflexology and a scalp massage. Breathing treatment & Herbal infused hot towel with a foot detox herbal spray.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-couples-2.jpg",
         title: "Couples #2",
-        price: "1 Hour & 30 mins: $480.00",
+        price: "1 Hour: $342.00 | Upgrade to Deep Tissue: $392.00",
         desc:
           "Hot Stones & Aromatherapy. Performed in a serene setting, the duet massage encourages unity, connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Swedish Massages that help the two of you relax.With a Cooling Mint Eye mask. Including reflexology and a scalp massage. Breathing treatment, with a Herbal infused detox spray.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-couples-3.jpg",
         title: "Couples #3",
-        price: "1 Hour & 30 mins: $480.00",
+        price: "1 Hour & 30 mins: $522.00",
         desc:
           "Deep Tissue Massage, Hot Stone, Aromatherapy & Indian Scalp Massage. Performed in a serene setting,the duet massage encourages unity, connection and collective calm. Ease stress and unwind as two therapists perform side-by-side Deep Tissue Massages that help the two of you relax.With a Cooling Eye mask. With our own Herbal breathing treatment and herbal foot detox spray.",
         sm: 12,
-        xl: 4
-      }
-    ]
+        xl: 4,
+      },
+    ],
   },
   {
     row: [
@@ -44,7 +44,7 @@ const deluxeCouplesMassages = [
         desc:
           "1 Hour and 35 Minutes of Swedish Massage, Hot Stones, Reflexology, Aromatherapy, and a Pressure Point Release Rain Drop Facial and Pearl Mask.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-deluxe-silk-spine.jpg",
@@ -53,7 +53,7 @@ const deluxeCouplesMassages = [
         desc:
           "Gliding around spine very gently pushing tired knotted up muscles away from the spine gently caressing the nerves in the surrounding are to relive and relax your central nervous system. Allowing your central nervous system to flow freely and unblocked. Better sleep and fewer headaches along with lowered anxiety. Overall mood improver!",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-deluxe-4-handed.jpg",
@@ -62,10 +62,10 @@ const deluxeCouplesMassages = [
         desc:
           "A therapeutic, duet massage is performed by two therapists whose coordinated movements induce relaxation and ease tension. To promote self balance and clarity. A soothing scalp massage and reflexology will leave you with a sense of calm and well-being. The organic,balancing massage oil used during treatment soothes the senses and nourishes the skin. Emerge renewed, refreshed and relaxed.",
         sm: 12,
-        xl: 4
-      }
-    ]
-  }
+        xl: 4,
+      },
+    ],
+  },
 ];
 
 export default class DeluxeCouplesMassages extends Component {
--- src/components/services/Massages.js
+++ src/components/services/Massages.js
@@ -13,7 +13,7 @@ const massages = [
         desc:
           "Receive a 15 minute traditional Swedish massage that will help alleviate your stress, tension, pain or stiffness while increasing your energy level, alertness and creativity. Our professional Massage Therapists perform the massage in a specially designed ergonomic massage chair while you’re fully clothed, using traditional Swedish massage techniques.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-champagne.jpg",
@@ -22,7 +22,7 @@ const massages = [
         desc:
           " Absorb the rich, romantic, renewing bouquet of the Champagne and Rose Collection. The massage oil delivers smooth, long-lasting glide and is rich in nutrients. Perfect for couples, Valentine's Day or Mother's Day services. Paraben-Free.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-cold-stone.jpg",
@@ -31,20 +31,20 @@ const massages = [
         desc:
           "Cold stone attributes that minimizes soreness, pulls excess heat from the body, stregthen the body on a cellular level, easily remove heat from the body. It also provides a deep state of relaxation, reduction of swelling or inflammation due to scar tissue, trauma or injuries to muscles. Cold stone helps alleviate anxiety, depression, PMS, sinusitis, high  blood pressure, and fatigue.",
         sm: 12,
-        xl: 4
-      }
-    ]
+        xl: 4,
+      },
+    ],
   },
   {
     row: [
       {
         icon: "/resources/services/massages/massage-deep-tissue.jpg",
         title: "Deep Tissue",
-        price: "30 Minutes: $50.00 | 1 Hour: $112.00 | 1 Hour ½: $167.00",
+        price: "30 Minutes: $67.00 | 1 Hour: $122.00 | 1 Hour ½: $167.00",
         desc:
           "A style of massage that is designed to get into the connective tissue of the body, rather than just the surface muscles. Many clients have a more intense experience with a deep tissue massage, but also feel that it is more beneficial, because it addresses deep-seated muscle pains. Like other types of massage,deep tissue is most beneficial when undertaken on a regular basis, so that therapist and client can work together to correct long term problems, relax the body, and prevent injury.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-himalayan.jpg",
@@ -53,7 +53,7 @@ const massages = [
         desc:
           "An innovative, detoxifying, healing technique using warm salt stones to ground and balance the body’s electromagnetic field, entral nervous system and meridians.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-hot-stone.jpg",
@@ -62,9 +62,9 @@ const massages = [
         desc:
           "Hot stone helps by improve overall mental, spiritual,and physical wellness. Native Americans used hot stones to detoxify and to find harmony and balance. Basalt stones, polished, hardened lava rocks, are heated by hot water. The heated stones are then placed strategically along pressure points to help melt away tension creating a calming atmosphere. Stress, deadlines, anxiety, and other life annoyances disappear long enough for your mind to relax at the deepest level. Over all it creates a sense of well being.",
         sm: 12,
-        xl: 4
-      }
-    ]
+        xl: 4,
+      },
+    ],
   },
   {
     row: [
@@ -75,7 +75,7 @@ const massages = [
         desc:
           "Medical massage involves therapeutic assessment, manipulation, and movement of the locomotor soft tissues to reduce or eliminate pain or dysfunction. A unique multidisciplinary approach is utilized to restore structural balance throughout the body, which allows focus on prevention and rehabilitation of musculoskeletal dysfunctions, chronic pain and sports injuries. Primary modalities include functional assessment, myofascial release, neuromuscular therapy,scar tissue mobilization techniques, neuromuscular re-education, PNF stretching, strengthening, and  specific client home-care protocols.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-myofascial.jpg",
@@ -84,7 +84,7 @@ const massages = [
         desc:
           "The focus of myofascial release is on the myofascial, the web of interconnected connective tissue that wraps around and surrounds every muscle, bone, nerve, blood vessel and organ in the body. Trauma, inflammatory responses, and/or surgical procedures create Myofascial restrictions that can produce tensile pressures of approximately 2,000 pounds per square inch on pain sensitive structures that do not show up in many of the standard tests x-rays, myelograms, CAT scans, electromyography, etc.)",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-indian-scalp.jpg",
@@ -93,9 +93,9 @@ const massages = [
         desc:
           "The Indian scalp massage improves blood flow to the head and neck, which increases the distribution of nutrients and encourages healing throughout the body, beneficial for relieving stress and tension, improving concentration, increasing joint mobility and flexibility in the neck and shoulders, improving blood circulation and lymphatic flow, reducing fatigue, relieving insomnia, headaches, migraines and sinusitis. Enjoy a deeply calming and relaxing treatment that will also leave you feeling energized and revitalized!",
         sm: 12,
-        xl: 4
-      }
-    ]
+        xl: 4,
+      },
+    ],
   },
   {
     row: [
@@ -106,7 +106,7 @@ const massages = [
         desc:
           "Manual Lymphatic Drainage is a form of massage that stimulates the lymphatic system with gentle massaging strokes. The light rhythmical massage encourages the lymphatic system to eliminate metabolic waste products, excess fluid and bacteria, promotes detoxification, facilitates healing and supports the immune system. Studies have shown that it helps stop and reduce the production of cellulite after a series of treatments along with a healthy diet and exercise.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-pregnancy.jpg",
@@ -115,7 +115,7 @@ const massages = [
         desc:
           "(Please consult with your doctor during your first trimester to get approval for massage.) Enhance your special time with massage therapy. Massage can help alleviate tension, fluid retention, fatigue and stress and improve circulation in the feet, legs, lower back, hips and neck. You will be lying comfortably on your side for a relaxing Swedish massage. Before making an appointment, please check with your physician to make sure that massage is right for you.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-reflexology.jpg",
@@ -124,9 +124,9 @@ const massages = [
         desc:
           "A holistic therapy that aims to restore and maintain the body’s natural balance and encourage healing. According to Chinese medicine, the sensory nerves of the internal organs throughout the body are mainly gathered around the soles of the feet. Through application of pressure on these reflexes without the use of tools, reflexology is proven to produce potent therapeutic benefits for a wide range of health conditions. It can also produce one of the most sublime relaxation experiences while it rejuvenates and revitalizes all aspects of our body and being.",
         sm: 12,
-        xl: 4
-      }
-    ]
+        xl: 4,
+      },
+    ],
   },
   {
     row: [
@@ -137,16 +137,16 @@ const massages = [
         desc:
           "Originally designed for athletes, to help prepare their bodies for optimal performance, recover after a big event, or function well during training. However it is useful for anyone with chronic pain, injury or range-of-motion issues. Sports Massage Emphasizes Prevention and Healing of injuries to the Muscles and Tendons.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-swedish.jpg",
         title: "Swedish Massage",
-        price: "30 Minutes: $52.00 | 1 Hour: $87.00 | 1 Hour ½: $130.00",
+        price: "30 Minutes: $62.00 | 1 Hour: $97.00 | 1 Hour ½: $130.00",
         desc:
           "This classic massage treatment is also known as a Relaxation Massage and the goal is to relax the entire body through various techniques such as long, flowing strokes using light to medium pressure. Your session will address such issues as stress, tension and mild to moderate muscle discomfort. Swedish massage is often recommended for first time massage clients and is excellent for improving circulation and flexibility while easing tension.",
         sm: 12,
-        xl: 4
+        xl: 4,
       },
       {
         icon: "/resources/services/massages/massage-trigger-point.jpg",
@@ -155,9 +155,9 @@ const massages = [
         desc:
           "A bodywork technique that involves the applying of pressure to tender muscle tissue in order to relieve pain and dysfunction in other parts of the body. The purpose of trigger point therapy is to eliminate pain and to re-educate the muscles into pain-free habits. After several treatments, the swelling and stiffness of neuromuscular pain is reduced; range of motion is increased; tension is relieved; and circulation,  flexibility and coordination are improved.",
         sm: 12,
-        xl: 4
-      }
-    ]
+        xl: 4,
+      },
+    ],
   },
   {
     row: [
@@ -168,10 +168,10 @@ const massages = [
         desc:
           "Generally red wine when used in skin care products fights free radicals, stimulates blood circulation, rejuvenates, increases skin elasticity, eases the nerves, firms, whitens, and leaves your skin bright and glowing These stimulating properties also softens wrinkles and fine lines and is altogether very useful in battling the effects of aging skin. It is thought that the ingredients in the wine massage numbs nerve endings and so allows muscles to reach a greater state of relaxation. This relaxation then allows a vasodilation of the blood vessels that surround muscle tissue and means more healing nutrients can get to the damaged tissue.",
         sm: 12,
-        xl: 4
-      }
-    ]
-  }
+        xl: 4,
+      },
+    ],
+  },
 ];
 
 export default class Massages extends Component {
