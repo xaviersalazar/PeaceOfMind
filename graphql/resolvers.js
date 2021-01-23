9c72c5c992103861885269ca40d62d572da0dfa2
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-23 13:42:13 -0500

    adding service models, typedefs, and resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,14 @@
 import User from "../models/user";
+import Service from "../models/service";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { isUndefined } from "lodash";
 
 require("dotenv").config();
 
 const resolvers = {
   Query: {
-    async login(_, { username, password }) {
+    login: async (_, { username, password }) => {
       const user = await User.findOne({ username: username });
 
       if (!user) throw new Error(`Username ${username} does not exist`);
@@ -19,15 +21,23 @@ const resolvers = {
 
       return { token, password: null, ...user._doc };
     },
-    async verifyToken(_, { token }) {
+    verifyToken: async (_, { token }) => {
       const decoded = jwt.verify(token, process.env.SECRET_KEY);
       const user = await User.findOne({ _id: decoded.id });
 
       return { ...user._doc, password: null };
     },
+    getService: async (_, { id }) => {
+      const service = await Service.findById({ id });
+
+      if (!service) throw new Error(`Service with ID ${id} does not exist`);
+
+      return service;
+    },
   },
   Mutation: {
-    async createUser(_, { username, password }) {
+    createUser: async (_, { userInput }) => {
+      const { username, password } = userInput;
       const existingUser = await User.findOne({ username });
 
       if (existingUser) throw new Error(`User ${username} already exists!`);
@@ -49,6 +59,38 @@ const resolvers = {
       const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
 
       return { token, password: null, ...user._doc };
+    },
+    createService: async (_, { serviceInput }) => {
+      const { title, description, price } = serviceInput;
+      const service = new Service(
+        {
+          title,
+          description,
+          price,
+        },
+        (err) => {
+          if (err) throw err;
+        }
+      );
+
+      service.save();
+
+      return service;
+    },
+    updateService: async (_, { id, serviceInput }) => {
+      const { title, description, price } = serviceInput;
+      const update = Object.assign(
+        {},
+        isUndefined(title) ? null : { title },
+        isUndefined(description) ? null : { description },
+        isUndefined(price) ? null : { price }
+      );
+
+      const service = await Service.findOneAndUpdate({ _id: id }, update, {
+        new: true,
+      });
+
+      return service;
     },
   },
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -7,18 +7,34 @@ const typeDefs = gql`
     token: String!
   }
 
+  type Service {
+    _id: ID!
+    title: String
+    description: String
+    price: String
+  }
+
   input UserInput {
     username: String!
     password: String!
   }
 
+  input ServiceInput {
+    title: String
+    description: String
+    price: String
+  }
+
   type Query {
     login(username: String!, password: String!): User
     verifyToken(token: String!): User
+    getService(id: ID!): Service
   }
 
   type Mutation {
     createUser(userInput: UserInput): User
+    createService(serviceInput: ServiceInput): Service
+    updateService(id: ID!, serviceInput: ServiceInput): Service
   }
 `;
 
