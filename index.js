b4a2942340b58e62a7a83e99e90b2f92bd8e1e0e
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-22 22:56:15 -0500

    adding mongodb connection

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { graphqlHTTP } from "express-graphql";
+import mongoose from "mongoose";
 import cors from "cors";
 
+require("dotenv").config();
+
 const app = express();
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@peaceofmindcluster.ea7su.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-app.use(cors());
+app.use(cors(), bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`⚡️ Server is running at http://localhost:${PORT}`);
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: {},
+    rootValue: {},
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`⚡️ Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
