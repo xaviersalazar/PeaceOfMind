9c2d0c0a7d1da385c3c32a9c42b3daa7ee792afd
Xavier Salazar <salazar.xavier26@gmail.com>
2021-01-22 23:33:58 -0500

    adding login to auth resolver

--- graphql/resolvers/auth/auth.js
+++ graphql/resolvers/auth/auth.js
@@ -10,9 +10,7 @@ export const createUser = async (args) => {
 
     const existingUser = await User.findOne({ username });
 
-    if (existingUser) {
-      throw new Error(`User ${username} already exists!`);
-    }
+    if (existingUser) throw new Error(`User ${username} already exists!`);
 
     const hashedPass = await bcrypt.hash(password, 10);
 
@@ -27,6 +25,26 @@ export const createUser = async (args) => {
     );
 
     user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+
+    return { token, password: null, ...user._doc };
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const login = async (args) => {
+  try {
+    const { username, password } = args;
+
+    const user = await User.findOne({ username: username });
+
+    if (!user) throw new Error(`Username ${username} does not exist`);
+
+    const isPasswordValid = await bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordValid) throw new Error("Password invalid");
 
     const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
 
